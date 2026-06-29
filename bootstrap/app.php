637c6d714a85f7090f20615f7337c22644bf5c6f
syqrhnn <syqrhnn@gmail.com>
2026-06-29 16:43:34 +0700

    fix: trust all proxies to enforce HTTPS on Railway

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+        
         $middleware->alias([
             'auth.custom' => \App\Http\Middleware\AuthMiddleware::class,
             'role' => \App\Http\Middleware\RoleMiddleware::class,
